71ddcf0157aab33a64ba4520f5b6e7c38e5eed66
Doug Hodson <doug@sidechannel.net>
2026-06-04 09:28:54 -0400

    updating premake

--- vs2013.bat
+++ vs2013.bat
@@ -33,6 +33,6 @@ del /S/Q *.log *.pdb *.exe *.idb
 cd ..
 
 echo on
-tools\premake\premake5 --os=windows --file=premake/premake.lua vs2013 %ARGUMENTS%
+tools\premake\premake5-dev-8693as0947d1 --os=windows --file=premake/premake.lua vs2013 %ARGUMENTS%
 
 cd /d %CURRENT_DIRECTORY%
